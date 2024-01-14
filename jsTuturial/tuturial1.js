a226158b4a3b1cd9b890e96daa0db9fc05204908
suman sorav <112455418+sumansorav123@users.noreply.github.com>
2024-01-14 04:48:51 +0000

    tuturial

--- jsTuturial/tuturial1.js
+++ jsTuturial/tuturial1.js
@@ -1,1 +1,20 @@
-console.log("hello")
+// variable decleration
+// There are four way for variable decleration in javascript
+
+// first way "const" keyword ( when we use const type we can't changed the value) 
+const Id = 6773;
+// second way "let" keyword (in this keyword  are changed the value)
+let email = "youtube@gmail.com";
+// third way "var"keyworde ( in this keyword we are changed the value but when we use "var" they are create a may problem in scope{} )
+var name = "webdeveloper";
+// fourth way " " (in this way we are not initlize any type )
+address = "India";
+
+// we try to change the all value
+// const Id = 2345; // (no change)
+email = "gmail.com"
+name = "developer";
+address = "Coding";
+
+// we call in table form
+console.table([Id,email,name,address]);
